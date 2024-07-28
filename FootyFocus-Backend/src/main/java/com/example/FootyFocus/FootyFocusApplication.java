020ccbdb76a4acf4a9732a2eb70d655a8e4bd26a
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-07-28 09:25:51 +0200

    FootyFocus backend first commit

---
+++ FootyFocus-Backend/src/main/java/com/example/FootyFocus/FootyFocusApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootyFocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootyFocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootyFocusApplication.class, args);
+	}
+
+}
