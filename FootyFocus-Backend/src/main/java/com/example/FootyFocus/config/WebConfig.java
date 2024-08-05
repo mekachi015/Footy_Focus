a2fc6d7ce1cacdc9d9d90db624ee4e55dd65df96
mekachi015 <abielmakoti@gmail.com>
2024-08-05 14:23:49 +0200

    backend working jwt

--- FootyFocus-Backend/src/main/java/com/example/FootyFocus/config/WebConfig.java
+++ FootyFocus-Backend/src/main/java/com/example/FootyFocus/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200","http://localhost:4200/player" , "http://localhost:4200/league-standings", "http://localhost:4200/player-wishlist" )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
