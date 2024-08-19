a7d4540da97aa997ea85deaa712ab4b01d0017fd
mekachi015 <abielmakoti@gmail.com>
2024-08-19 15:37:41 +0200

    working player infomation backedn

--- FootyFocus-Backend/src/main/java/com/example/FootyFocus/controller/PlayerInfoController.java
+++ FootyFocus-Backend/src/main/java/com/example/FootyFocus/controller/PlayerInfoController.java
@@ -4,15 +4,13 @@ import com.example.FootyFocus.entity.PlayerInfo;
 import com.example.FootyFocus.service.APIServices.SearchPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/players")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PlayerInfoController {
 
 
